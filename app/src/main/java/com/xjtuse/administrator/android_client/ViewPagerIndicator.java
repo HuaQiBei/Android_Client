b4140290ba2890961dfd9e091945d146514861af
Elvira <lixi.csu@gmail.com>
2016-08-04 18:15:36 +0800

    First

---
+++ app/src/main/java/com/xjtuse/administrator/android_client/ViewPagerIndicator.java
@@ -0,0 +1,18 @@
+package com.xjtuse.administrator.android_client;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Elvira on 2016/8/4.
+ */
+public class ViewPagerIndicator extends LinearLayout {
+    public ViewPagerIndicator(Context context) {
+        this(context,null);
+    }
+
+    public ViewPagerIndicator(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
