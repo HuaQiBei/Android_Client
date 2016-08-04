98a6032425e9024ea6518525c861de1700519ade
jiangwu45 <578880579@qq.com>
2016-08-04 12:07:47 +0800

    SingleFragmentActivity xml

--- app/src/main/java/com/xjtuse/administrator/android_client/SingleFragmentActivity.java
+++ app/src/main/java/com/xjtuse/administrator/android_client/SingleFragmentActivity.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 /**
  * Created by WuJiang on 2016/8/1.
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
@@ -19,7 +20,6 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fragment);
-
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
